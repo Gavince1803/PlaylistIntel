8fe4e0440a81dea048695206d4fc9c7c204ec649
Gavince1803 <sonicvincenzo@gmail.com>
2025-07-30 17:41:58 -0400

    Free Premium state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -63,7 +63,7 @@ export default function Sidebar({ isOpen, setIsOpen }: SidebarProps) {
         className={`
           fixed inset-y-0 left-0 z-50 w-64 bg-[#191414] shadow-lg transform transition-transform duration-300 ease-in-out
           ${isOpen ? 'translate-x-0' : '-translate-x-full'}
-          lg:translate-x-0 lg:static lg:inset-0
+          lg:translate-x-0
         `}
         ref={sidebarRef}
         tabIndex={isOpen ? 0 : -1}
